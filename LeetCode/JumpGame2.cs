2eb60eb37eb7c6dd245259718badb7fdf8765c7e
Ruc-X <1486293375@qq.com>
2020-03-30 16:50:05 +0800

    LeetCode题解

---
+++ LeetCode/JumpGame2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace JumpGame2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 2, 3, 1, 1, 4 };
+            int length = nums.Length;
+            int end=0,times=0;
+            int maxpos = 0;
+            for(int i=0;i<length-1;i++)
+            {
+                maxpos = Math.Max(maxpos, nums[i] + i);
+                if(i==end)
+                {
+                    end = maxpos;
+                    times++;
+                }
+            }
+            Console.WriteLine("{0}", times);
+        }
+    }
+}
