2b5e9dd044024332253a58d8a61852c778f13337
Ruc-X <1486293375@qq.com>
2020-04-05 15:08:46 +0800

    LeetCode题解

---
+++ LeetCode/FirstMissingPositive.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeAns
+{
+    class FirstMissingPositive
+    {
+        static int Main()
+        {
+            int[] nums;
+            nums = new int[] {1,2,3};
+            int length = nums.Length;
+            int[] appear;
+            appear = new int[length];
+            for(int i=0;i<length;i++)
+            {
+                if (nums[i]<=length&&nums[i]>=1)
+                {
+                    appear[nums[i] - 1] = 1;
+                }
+            }
+            for(int i=0;i<length;i++)
+            {
+                if (appear[i] == 0)
+                {
+                    Console.WriteLine("{0}", i + 1);
+                    return (i + 1);
+                }
+            }
+            Console.WriteLine("{0}", length + 1);
+            return (length + 1);
+        }
+    }
+}
